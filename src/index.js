9d41b60756ef50a8dbcdf1ec5a3feee717894110
anil kadam <anilkadam210@gmail.com>
2020-02-18 00:14:33 +0530

    Added /users endpoint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
+const mockUsers = [];
+
 app.get('/', (req, res)=>{
     res.send("Hello World!");
+});
+
+app.get('/users', (req, res) => {
+    res.send(mockUsers);
 });
 
 app.listen(3000, ()=>{
