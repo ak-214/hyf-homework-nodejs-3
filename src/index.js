8909687e5c67f1cc87a4771176f79f04ef882981
anil kadam <anilkadam210@gmail.com>
2020-02-17 22:19:35 +0530

    Fixing test case

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.json({
-        body: 'Hello World'
-    })
+    res.send("Hello World0");s
 });
 
 app.listen(3000, ()=>{
