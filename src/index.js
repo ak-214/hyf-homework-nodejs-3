91d5f4c219259104aae331447e356f8ae831d939
ak-214 <41534829+ak-214@users.noreply.github.com>
2020-02-18 00:27:00 +0530

    Revert "Step 2 (#4)"
    This reverts commit 3531afcd6f290e88f856ed3748bfece9c930bab0.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const app = express();
 
-const mockUsers = [];
-
 app.get('/', (req, res)=>{
     res.send("Hello World!");
-});
-
-app.get('/users', (req, res) => {
-    res.send(mockUsers);
 });
 
 app.listen(3000, ()=>{
